b75646abcc790ff3e5738a0be4fdfc4cfe34f64b
dddddora <55509410+dddddora@users.noreply.github.com>
2020-03-12 19:38:42 +0100

    bzzzt

---
+++ pp.py
@@ -0,0 +1,16 @@
+def typeOfAnimal(aa):
+    """returns type of animal"""
+    if aa == "meow":
+        return "cat"
+    elif aa == "woof":
+        return "dog"
+    else:
+        return "heck"
+
+
+print(typeOfAnimal("meow"))
+print(typeOfAnimal("woof"))
+print(typeOfAnimal("bzzzttt"))
+
+
+
