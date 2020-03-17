61525590f9807b1a7bc86b3ee0921715a551a452
dddddora <dora.dasovic.01@gmail.com>
2020-03-17 19:47:54 +0100

    input and print

---
+++ mrmr.py
@@ -0,0 +1,14 @@
+age = input("Plox enter your age: ") #if we give text to input it will print it
+
+print("Plox enter your name")
+name = input()
+
+a = name + " ima " + str(age) + " godina"
+
+b = "{0} ima {1} godina".format(name,age)
+
+c = f"{name} ima {age} godina"
+
+print(a)
+print(b)
+print(c)
