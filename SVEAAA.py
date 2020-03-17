20e1f43dc68be62bd6c0ba091f2f8d043fba51e5
dddddora <dora.dasovic.01@gmail.com>
2020-03-17 20:16:32 +0100

    evilInput

---
+++ SVEAAA.py
@@ -0,0 +1,13 @@
+def evilInput(text=""):
+    if text == "":
+        print("buahahahah: ", end='')
+    else: 
+        print(text)
+    return input()
+
+
+a = evilInput()
+print(a)
+
+print("lalala", end="-")
+print("lalala")
